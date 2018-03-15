6057b9a834e4189e5a4316249ef077bcdd57e036
rooksandkings <vagrant@vagrant.vm>
2018-03-15 20:24:04 +0000

    Moved most of Flash messages to header, fix picture not loading on newly minted menus, fix adding to menu not working

--- project.py
+++ project.py
@@ -364,19 +364,26 @@ def showMenu(restaurant_id):
 # Create a new menu item
 @app.route('/restaurant/<int:restaurant_id>/menu/new/', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
+    flash('Load')
     if 'username' not in login_session:
         return redirect('/login')
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     if login_session['user_id'] != restaurant.user_id:
         return "<script>function myFunction() {alert('You are not authorized to add menu items to this restaurant. Please create your own restaurant in order to add items.');}</script><body onload='myFunction()'>"
-        if request.method == 'POST':
-            newItem = MenuItem(name=request.form['name'], description=request.form['description'], price=request.form[
-                               'price'], course=request.form['course'], restaurant_id=restaurant_id, user_id=restaurant.user_id)
+    if request.method == 'POST':
+            flash('POSTED')
+            newItem = MenuItem(name=request.form['name'], 
+                                description=request.form['description'], 
+                                price=request.form['price'], 
+                                course=request.form['course'], 
+                                restaurant_id=restaurant_id, 
+                                user_id=restaurant.user_id)
             session.add(newItem)
             session.commit()
             flash('New Menu %s Item Successfully Created' % (newItem.name))
             return redirect(url_for('showMenu', restaurant_id=restaurant_id))
     else:
+        flash('New Menu Item NOT Successfully Created')
         return render_template('newmenuitem.html', restaurant_id=restaurant_id)
 
 # Edit a menu item
